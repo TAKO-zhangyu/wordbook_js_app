5b1ccbe6d20fc1c2a70385a9303eb0f6af650880
Tako <takoforme@gmail.com>
2021-01-11 08:30:23 +0900

    単語情報を変数で管理するよう変更

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 $(function() {
+  var words = { prevent: '防ぐ' };
+    $(function(){
+      $.each(words, function(key, value){
+          $("#card-front").append(key);
+          $("#card-back").append(value);
+      })
+    });
+
   $('#card-front').on("click", function () {
       $('#card-front').hide();
       $('#card-back').show();
