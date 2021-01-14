00e3af2dabd6559cea3996af811a82223334f657
Tako <takoforme@gmail.com>
2021-01-14 09:12:22 +0900

    配列に単語オブジェクトを移動

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 $(function() {
-  var words = { 
-    prevent: '防ぐ', 
-    humble: '謙虚な', 
-    vaccine: 'ワクチン' 
-  };
+  // 単語
+  var words = [
+    {
+      en: 'prevent',
+      ja: '防ぐ'
+    },
+    {
+      en: 'humble',
+      ja: '謙虚な'
+    },
+    {
+      en: 'vaccine',
+      ja: 'ワクチン'
+    }
+  ]
 
-  $(function(){
-    $.each(words, function(key, value){
-        $("#card-front").append(key);
-        $("#card-back").append(value);
-      })
-    });
+  // 単語を取得する
+  $("#card-front").text(words[0].en)
+  $("#card-back").text(words[0].ja)
 
+  // 次のカードを表示する
+  $('#btn').on("click", function () {
+    $('#card-front').text(words[1].en)
+  });
+
+  // 表のカードから裏のカードに切り替える
   $('#card-front').on("click", function () {
       $('#card-front').hide();
       $('#card-back').show();
