1b6b805153dead147d6d1334d1b4d5cda37a094c
Tako <takoforme@gmail.com>
2021-01-20 00:55:04 +0900

    配列を個別に取得

--- index.js
+++ index.js
@@ -15,18 +15,21 @@ $(function() {
     }
   ]
 
-  // 単語を取得する
-  for (var i = 0; i < words.length; i++) {
-    $("#card-front").text(words[i].en)
-    $("#card-back").text(words[i].ja)
+  // 最初の単語を表示する
+    $("#card-front").text(words[0].en)
+    $("#card-back").text(words[0].ja)
 
+    var i = 0;
 
     // 次のカードを表示する
     $('#btn').on("click", function () {
+      $('#card-back').hide();
+      $('#card-front').show();
+      i++;
       $('#card-front').text(words[i].en)
+      $('#card-back').text(words[i].ja)
     });
-  }
-
+    
   // 表のカードから裏のカードに切り替える
   $('#card-front').on("click", function () {
       $('#card-front').hide();
